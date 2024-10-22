c631a7e34bd8f05af3a793d8b6ec1a48524c1008
Denis Willian <deniswrs11@gmail.com>
2024-10-22 17:34:11 -0300

    formatação

--- teste 03/assets/js/main.js
+++ teste 03/assets/js/main.js
@@ -1,19 +1,17 @@
 const elementos = [
-    {tag: 'p', texto: 'Frase 1'},
-    {tag: 'div', texto: 'Frase 2'},
-    {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'section', texto: 'Frase 4'},
+  { tag: "p", texto: "Frase 1" },
+  { tag: "div", texto: "Frase 2" },
+  { tag: "footer", texto: "Frase 3" },
+  { tag: "section", texto: "Frase 4" },
 ];
 
-
 for (let i = 0; i < elementos.length; i++) {
-    const {tag, texto} = elementos[i];
-    
-    const conteudo = document.querySelector('.container');
+  const { tag, texto } = elementos[i];
 
-    const novoConteudo = document.createElement(tag);
-    
-    novoConteudo.innerHTML = texto;
-    conteudo.appendChild(novoConteudo);
+  const conteudo = document.querySelector(".container");
 
+  const novoConteudo = document.createElement(tag);
+
+  novoConteudo.innerHTML = texto;
+  conteudo.appendChild(novoConteudo);
 }
